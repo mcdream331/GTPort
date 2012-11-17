19c0fc592d65047a4edb8b15396a87de1476b6d1
Fox Hu <phu6gt@gmail.com>
2012-11-17 12:24:49 -0500

    create acount

---
+++ php/create_accout.php
@@ -0,0 +1,26 @@
+<?php
+mysql_connect($host,$username,$password) or die( "Unable to connect");;
+mysql_select_db($database) or die( "Unable to select database");
+
+// When password and comfirm password are equal
+$sql_query = "INSERT INTO        User (Username, Password)
+VALUES        ($username, $password)";
+$sql_query1 = "INSERT INTO        Regular_User (Username)
+VALUES        ($username)";
+
+mysql_query ($sql_query);
+mysql_query ($sql_query1);  
+
+if ($usertype="Student")
+	$sql_query2 = "INSERT INTO        Student 	(Student_Username)
+	VALUES        ($username);"
+elseif ($usertype="Faculty")
+	$sql_query2 = "INSERT INTO        Faculty 	(Instructor_Username)
+	VALUES        ($username);"
+//else
+	//no value
+
+mysql_query ($sql_query2);
+
+mysql_close ();
+?>
