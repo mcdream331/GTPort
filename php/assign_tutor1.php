7ddf77af5dacf41a051e1cb74e42e5511a8cc801
Fox Hu <phu6gt@gmail.com>
2012-12-04 13:35:57 -0500

    assign_tutor1

--- php/assign_tutor1.php
+++ php/assign_tutor1.php
@@ -1,21 +1,37 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+session_start();
+?>
+<html>
+	<body>
+		<h1>
+			GTPort
+		</h1>
+		<?php
+		header("Access-Control-Allow-Origin: *");
+		
+		//establish connection
+		$link = mysql_connect("localhost", "root", "root");
+		if (!$link) {
+			die('Could not connect: ' . mysql_error());
+		}
+		mysql_select_db(cs4400) or die("Unable to select database");
 
-//establish connection
-$link = mysql_connect("localhost", "root", "root");
-if (!$link) {
-	die('Could not connect: ' . mysql_error());
-}
-mysql_select_db(cs4400) or die("Unable to select database");
+		//insert user
+		$sql_query = "SELECT Tutor_Username FROM Tutor WHERE Tutor = $tutorUsername";
+		$istutor = mysql_query($sql_query) or die('Select course title error' . mysql_error());
+		
+		$rowcount = mysql_num_rows($istutor);
+		if ($rowcount == 0) {
+			$sql_query1 = "INSERT INTO        Tutor (Tutor_Username) VALUES ($tutorUsername)";
+			mysql_query($sql_query1) or die('Select course title error' . mysql_error());
+		}
+		$sql_query2 = "INSERT INTO        Tutor_Course VALUES        ($tutorUsername, $title)";
+		mysql_query($sql_query2) or die('select tutor name error' . mysql_error());
 
-//insert user
-$sql_query = "SELECT DISTINCT Title FROM Section WHERE Instructor_Username = $username";
-$title = mysql_query($sql_query) or die('Select course title error' . mysql_error());
+		//close connection
+		mysql_close($link);
 
-$sql_query1 = "SELECT    Name FROM Regular_User JOIN Apply_Tutor ON Student_Username = Username WHERE    Title = $title";
-$result = mysql_query($sql_query1) or die('select tutor name error' . mysql_error());
-
-//close connection
-mysql_close($link);
-
-echo "succeed";
+		echo "succeed";
+		?>
+	</body>
+</html>
