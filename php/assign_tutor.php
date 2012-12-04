c28e5caace8212e5131cefc9220d4b6cb1a10484
Fox Hu <phu6gt@gmail.com>
2012-12-03 20:15:35 -0500

    tutor_log

--- php/assign_tutor.php
+++ php/assign_tutor.php
@@ -12,7 +12,7 @@ mysql_select_db(cs4400) or die("Unable to select database");
 $sql_query = "SELECT DISTINCT Title FROM Section WHERE Instructor_Username = $username";
 $title = mysql_query($sql_query) or die('Select course title error' . mysql_error());
 
-$sql_query1 = "SELECT    Name FROM Regular_User JOIN Apply_Tutor ON Student_Username = Username WHERE    Title = $title;";
+$sql_query1 = "SELECT    Name FROM Regular_User JOIN Apply_Tutor ON Student_Username = Username WHERE    Title = $title";
 $result = mysql_query($sql_query1) or die('select tutor name error' . mysql_error());
 
 //close connection
