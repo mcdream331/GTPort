d6ff99ea8deab1cf3c4f49fc4b5264626094e664
Zunhao Tang <reagantang@hotmail.com>
2012-12-04 00:50:58 -0500

    123

--- php/view_faculty_personal_information.php
+++ php/view_faculty_personal_information.php
@@ -30,15 +30,16 @@ while ($row1 = mysql_fetch_array($result1)) {
 }
 
 //Populate Drop Down for Course and Section
-$sql_query2 = "SELECT Title FROM Department_Course as C, Department as D WHERE D.Name = '$department' AND D.Dept_Id = C.Dept_Id";
+$sql_query2 = "SELECT Title FROM Department_Course as C, Department as D WHERE D.Name = '$department' AND D.Dept_Id = 'C.Dept_Id'";
 $result2 = $mysql_query($sql_query2) or die(mysql_error());
-$row2 = mysql_fetch_array($result2);
+echo "<select id='course'>"
+while($row2 = mysql_fetch_array($result2){
 $title = $row2['Title'];
-
-$sql_query3 = "SELECT Letter FROM Section WHERE Title = $title";
+$sql_query3 = "SELECT Letter FROM Section WHERE Title = '$title'";
 $result3 = $mysql_query($sql_query3) or die(mysql_error());
-echo "$result2";
-echo "$result3";
+echo "<select id = 'section'>"
+}
+echo "</select>";
 
 //After Entering Information new information
 $name = $_Get["name"];
@@ -55,22 +56,22 @@ $section = $_Get["section"];
 $research_interest = $_Get["research_interests"];
 
 //Update Personal Information
-$sql_query4 = "UPDATE Regular_User	Name = $name, Gender = $gender, DOB = $dob, Email_ID = $email, Contact_Number = $contact_number, Address = $address, Permanent_Address=$permanent_address WHERE Username = $username"
+$sql_query4 = "UPDATE Regular_User	Name = '$name', Gender = '$gender', DOB = '$dob', Email_ID = '$email', Contact_Number = '$contact_number', Address = '$address', Permanent_Address = '$permanent_address' WHERE Username = '$username'"
  mysql_query($sql_query4)or die (mysql_error());
 
 //Update Department Information
-$sql_query5 = "SELECT	DeptId FROM Department WHERE Department.Name = $department";
-$sql_query6 = "UPDATE Faculty SET Position = $position, Dept_Id = $Dept_Id WHERE Instructor_Username = $username";
+$sql_query5 = "SELECT	DeptId FROM Department WHERE Department.Name = '$department'";
+$sql_query6 = "UPDATE Faculty SET Position = '$position', Dept_Id = '$Dept_Id' WHERE Instructor_Username = '$username'";
 mysql_query($sql_query5) or die(mysql_error());
 mysql_query($sql_query6) or die(mysql_error());
 
 //Modify Course and Section
-$sql_query7 = "UPDATE Section SET Instructor_Username = $Username WHERE Letter = $Letter And Title = $Title";
+$sql_query7 = "UPDATE Section SET Instructor_Username = '$Username' WHERE Letter = '$Letter' And Title = '$Title'";
 mysql_query($sql_query7) or die(mysql_error());
 
 //Update Research Interest
-$sql_query8 = "DELETE FROM Faculty_Research_Interest WHERE Instructor_Username = $username";
-$sql_query9 = "INSERT INTO Faculty_Research_Interest (Instructor_Username, Research_Interest) VALUES Instructor_Username = $Username, Rearch_Interests = $Research_Interests";
+$sql_query8 = "DELETE FROM Faculty_Research_Interest WHERE Instructor_Username = '$username'";
+$sql_query9 = "INSERT INTO Faculty_Research_Interest (Instructor_Username, Research_Interest) VALUES Instructor_Username = '$Username', Rearch_Interests = '$Research_Interests'";
 mysql_query($sql_query8) or die(mysql_error());
 mysql_query($sql_query9) or die(mysql_error());
 ?>
