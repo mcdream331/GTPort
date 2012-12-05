3653dcfaaa327233285fcb568644bd1fe421b646
mcdream331 <mcdream331@gmail.com>
2012-12-04 20:19:14 -0500

    find tutor finish

--- php/find_tutor_1.php
+++ php/find_tutor_1.php
@@ -17,13 +17,19 @@ session_start();
 		}
 		mysql_select_db(cs4400) or die("Unable to select database");
 		
-		$myResult='';
+		echo "Search Result:<br>";
+		$myResult='<table><tr>
+					<th>Course Code</th>
+					<th>Course Name</th>
+					<th>Tutor Name</th>
+					<th>Tutor Email</th>
+					</tr>';
 		
 		if ($courseCode != '') {
 			$sql_query = "SELECT	Title, Code, Name, Email_Id
 					FROM		(Course_Code NATURAL JOIN Tutor_Course) JOIN Regular_User ON
 					Tutor_Username = Username
-					WHERE	Course_Code ='$courseCode');";
+					WHERE	Code ='$courseCode';";
 
 			$queryresult = mysql_query($sql_query) or die('error ' . mysql_error());
 			$rowcount = mysql_num_rows($queryresult);
@@ -31,34 +37,32 @@ session_start();
 			$i = 0;
 			while ($i < $rowcount) {
 				$Title = mysql_result($queryresult, $i, "Title");
-				$Code = mysql_result($queryresult, $i, "Code");
+				//$Code = mysql_result($queryresult, $i, "Code");
 				$Name = mysql_result($queryresult, $i, "Name");
 				$Email_Id = mysql_result($queryresult, $i, "Email_Id");
 				
-				echo "title:".$Title;
-				echo "code:".$Code;
-				echo "name:".$Name;
-				echo "email:".$Email_Id;
-
-				$myResult = $myResult . "<tr>
+				// echo "title:".$Title;
+				// echo "code:".$courseCode;
+				// echo "name:".$Name;
+				// echo "email:".$Email_Id;
+				$myResult .= "<tr>
+			<td>" . $courseCode . "</td>
 			<td>" . $Title . "</td>
-			<td>" . $Code . "</td>
 			<td>" . $Name . "</td>
 			<td>" . $Email_Id . "</td>
 		</tr>";
 
 				$i += 1;
 			}
-		} else {
+		} 
+		else {
 			$sql_query = "SELECT	Title, Code, Name, Email_Id
 					FROM		(Course_Code NATURAL JOIN Tutor_Course) JOIN Regular_User ON
 					Tutor_Username = Username
-					WHERE	Course_Code LIKE CONCAT(��%��,$keyword,��%��);";
+					WHERE	Title LIKE CONCAT('%','$keyword','%');";
 
 			$queryresult = mysql_query($sql_query) or die('error ' . mysql_error());
-
-			$rowcount = mysql_result($queryresult);
-			$myResult;
+			$rowcount = mysql_num_rows($queryresult);
 			$i = 0;
 			while ($i < $rowcount) {
 				$Title = mysql_result($queryresult, $i, "Title");
@@ -66,9 +70,9 @@ session_start();
 				$Name = mysql_result($queryresult, $i, "Name");
 				$Email_Id = mysql_result($queryresult, $i, "Email_Id");
 
-				$myResult = $myResult . "<tr>
-			<td>" . $Title . "</td>
+				$myResult .= "<tr>
 			<td>" . $Code . "</td>
+			<td>" . $Title . "</td>
 			<td>" . $Name . "</td>
 			<td>" . $Email_Id . "</td>
 		</tr>";
@@ -77,10 +81,15 @@ session_start();
 			}
 
 		}
+		
+		$myResult .= "</table>
+		<br>Click button below to return to search page.<br>
+		<input type='button' value='Back' onclick='location.href=\"find_tutor.php\"'/>";
+		
+		echo $myResult;
 		//close connection
 		mysql_close($link);
 
-		echo "succeed";
 	?>
 	</body>
 </html>
